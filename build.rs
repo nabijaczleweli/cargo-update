ef4346c96b1e55bb837557520e6ea09d3f0d867e
nabijaczleweli <nabijaczleweli@gmail.com>
2016-11-16 23:52:04 +0100

    Embed the manifest on Windows, so no elevation is performed
    This would be much cleaner if the build scripts were actually fucking
    documented properly
    
    Gigantic thanks to @TheCatPlusPlus who saved the day once again
    (solution heavily based on
    https://github.com/TheCatPlusPlus/rust-manifest-test)
    
    Ref: #11
    Closes #13

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+use std::path::Path;
+use std::env;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        let out_dir = env::var("OUT_DIR").unwrap();
+
+        Command::new("windres")
+            .args(&["--input", "cargo-install-update-manifest.rc", "--output-format=coff", "--output"])
+            .arg(&format!("{}/cargo-install-update-manifest.res", out_dir))
+            .status()
+            .unwrap();
+
+        Command::new("ar")
+            .args(&["crs", "libcargo-install-update-manifest.a", "cargo-install-update-manifest.res"])
+            .current_dir(&Path::new(&out_dir))
+            .status()
+            .unwrap();
+
+        println!("cargo:rustc-link-search=native={}", out_dir);
+    }
+}
