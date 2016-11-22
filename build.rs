f60285f419184237e65e237adc607f296d342e27
nabijaczleweli <nabijaczleweli@gmail.com>
2016-11-22 07:51:21 +0100

    Rearrange to treat MSVC as a special case

--- build.rs
+++ build.rs
@@ -9,7 +9,13 @@ fn main() {
 
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    if cfg!(target_env = "gnu") {
+    if cfg!(target_env = "msvc") {
+        // `.res`es are linkable under MSVC as well as normal libraries, for w/e reason.
+        Command::new("rc")
+            .args(&["/fo", &format!("{}/cargo-install-update-manifest.lib", out_dir), "cargo-install-update-manifest.rc"])
+            .status()
+            .unwrap();
+    } else {
         Command::new("windres")
             .args(&["--input", "cargo-install-update-manifest.rc", "--output-format=coff", "--output"])
             .arg(&format!("{}/cargo-install-update-manifest.res", out_dir))
@@ -19,19 +25,6 @@ fn main() {
         Command::new("ar")
             .args(&["crs", "libcargo-install-update-manifest.a", "cargo-install-update-manifest.res"])
             .current_dir(&Path::new(&out_dir))
-            .status()
-            .unwrap();
-    }
-
-    if cfg!(target_env = "msvc") {
-        // We'll create a `.res` file here, but name it as `.lib`,
-        // so that it can be found and linked correctly.
-        // Yes, `.res` may be linked directly, like `.lib` and `.o`.
-        // See `#[link(name="cargo-install-update-manifest")]` in main.rs.
-        let out_res = format!("{}\\cargo-install-update-manifest.lib", out_dir);
-
-        Command::new("rc")
-            .args(&["/fo", &out_res, "cargo-install-update-manifest.rc"])
             .status()
             .unwrap();
     }
