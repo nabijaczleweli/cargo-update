671172e55131596e547620cf74786b997bacdd6b
azyobuzin <azyobuzin@users.sourceforge.jp>
2017-01-23 15:37:54 +0900

    Improve code

--- build.rs
+++ build.rs
@@ -4,9 +4,11 @@ extern crate winreg;
 use std::process::Command;
 use std::path::{Path, PathBuf};
 use std::env;
+#[cfg(all(windows, target_env = "msvc"))]
+use winreg::enums::*;
 
 #[cfg(not(windows))]
-fn main() { }
+fn main() {}
 
 #[cfg(windows)]
 fn main() {
@@ -18,73 +20,67 @@ fn main() {
 #[cfg(all(windows, target_env = "msvc"))]
 fn compile_resource(out_dir: &str) {
     // `.res`es are linkable under MSVC as well as normal libraries.
+    let rc_path = find_windows_sdk_bin_dir().and_then(|mut x| {
+        x.push("rc.exe");
+        if x.exists() { Some(x) } else { None }
+    });
 
-    fn find_windows_sdk_bin_dir() -> Option<PathBuf> {
-        use winreg::enums::*;
-
-        #[derive(Clone, Copy)]
-        enum Arch {
-            X86,
-            X64,
-        }
-
-        // Windows 8 - 10
-        fn find_windows_kits_bin_dir(key: &str, arch: Arch) -> Option<PathBuf> {
-            let res = winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
-                .open_subkey_with_flags("SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots", KEY_QUERY_VALUE)
-                .and_then(|reg_key| reg_key.get_value::<String, _>(key));
-
-            match res {
-                Ok(root_dir) => {
-                    let mut p = PathBuf::from(root_dir);
-                    match arch {
-                        Arch::X86 => p.push("bin\\x86"),
-                        Arch::X64 => p.push("bin\\x64"),
-                    }
-                    if p.is_dir() { Some(p) } else { None }
-                }
-                Err(_) => None
-            }
-        }
-
-        // Windows Vista - 7
-        fn find_latest_windows_sdk_bin_dir(arch: Arch) -> Option<PathBuf> {
-            let res = winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
-                .open_subkey_with_flags("SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows", KEY_QUERY_VALUE)
-                .and_then(|reg_key| reg_key.get_value::<String, _>("CurrentInstallFolder"));
-
-            match res {
-                Ok(root_dir) => {
-                    let mut p = PathBuf::from(root_dir);
-                    match arch {
-                        Arch::X86 => p.push("Bin"),
-                        Arch::X64 => p.push("Bin\\x64"),
-                    }
-                    if p.is_dir() { Some(p) } else { None }
-                }
-                Err(_) => None
-            }
-        }
-
-        let arch = if env::var("TARGET").unwrap().starts_with("x86_64") { Arch::X64 } else { Arch::X86 };
-
-        find_windows_kits_bin_dir("KitsRoot10", arch)
-            .or_else(|| find_windows_kits_bin_dir("KitsRoot81", arch))
-            .or_else(|| find_windows_kits_bin_dir("KitsRoot", arch))
-            .or_else(|| find_latest_windows_sdk_bin_dir(arch))
-    }
-
-    let rc_exe = Path::new("rc.exe");
-    let rc_path = find_windows_sdk_bin_dir()
-        .and_then(|mut x| {
-            x.push(rc_exe);
-            if x.exists() { Some(x) } else { None }
-        });
-
-    Command::new(if let Some(ref x) = rc_path { x } else { rc_exe })
+    Command::new(rc_path.as_ref().map_or(Path::new("rc.exe"), Path::new))
         .args(&["/fo", &format!("{}/cargo-install-update-manifest.lib", out_dir), "cargo-install-update-manifest.rc"])
         .status()
         .expect("Are you sure you have RC.EXE in your $PATH?");
+}
+
+#[cfg(all(windows, target_env = "msvc"))]
+fn find_windows_sdk_bin_dir() -> Option<PathBuf> {
+    #[derive(Clone, Copy)]
+    enum Arch {
+        X86,
+        X64,
+    }
+
+    // Windows 8 - 10
+    fn find_windows_kits_bin_dir(key: &str, arch: Arch) -> Option<PathBuf> {
+        winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
+            .open_subkey_with_flags(r"SOFTWARE\Microsoft\Windows Kits\Installed Roots", KEY_QUERY_VALUE)
+            .and_then(|reg_key| reg_key.get_value::<String, _>(key))
+            .ok()
+            .and_then(|root_dir| {
+                let mut p = PathBuf::from(root_dir);
+                match arch {
+                    Arch::X86 => p.push(r"bin\x86"),
+                    Arch::X64 => p.push(r"bin\x64"),
+                }
+                if p.is_dir() { Some(p) } else { None }
+            })
+    }
+
+    // Windows Vista - 7
+    fn find_latest_windows_sdk_bin_dir(arch: Arch) -> Option<PathBuf> {
+        winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
+            .open_subkey_with_flags(r"SOFTWARE\Microsoft\Microsoft SDKs\Windows", KEY_QUERY_VALUE)
+            .and_then(|reg_key| reg_key.get_value::<String, _>("CurrentInstallFolder"))
+            .ok()
+            .and_then(|root_dir| {
+                let mut p = PathBuf::from(root_dir);
+                match arch {
+                    Arch::X86 => p.push("Bin"),
+                    Arch::X64 => p.push(r"Bin\x64"),
+                }
+                if p.is_dir() { Some(p) } else { None }
+            })
+    }
+
+    let arch = if env::var("TARGET").unwrap().starts_with("x86_64") {
+        Arch::X64
+    } else {
+        Arch::X86
+    };
+
+    find_windows_kits_bin_dir("KitsRoot10", arch)
+        .or_else(|| find_windows_kits_bin_dir("KitsRoot81", arch))
+        .or_else(|| find_windows_kits_bin_dir("KitsRoot", arch))
+        .or_else(|| find_latest_windows_sdk_bin_dir(arch))
 }
 
 #[cfg(all(windows, not(target_env = "msvc")))]
